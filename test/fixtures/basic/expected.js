5bfac68679a765dd6003b7201a8e4e8e15efe546
David Beaumont <david.beaumont@holidayextras.com>
2018-01-26 15:42:20 +0000

    Update package name

--- test/fixtures/basic/expected.js
+++ test/fixtures/basic/expected.js
@@ -2,6 +2,7 @@ const my_key_goes_here_en = 'english translation';
 const my_key_goes_here_de = 'deutche translation';
 import { localiseWithTranslations as t } from './lib/i18n';
 
+
 const translated = t('my_key_goes_here', {
   de: my_key_goes_here_de,
   en: my_key_goes_here_en
